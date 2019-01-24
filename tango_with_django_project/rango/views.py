eab1e24077c666a8fa58ada49cf02a087f3aaef5
Enda McVey <endamcvey@Endas-MacBook-2.local>
2019-01-24 17:54:57 +0000

    Fixed About us Links :D

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,34 +1,140 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-from rango.models import Category
-from rango.models import Page
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from rango.models import Category, Page
+
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()) )
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+    #last_visit_time = datetime.now()
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # update/set the visits cookie
+    request.session['visits'] = visits
+
+
+
+
+def index(request):
+    #context_dict = {'boldmessage': "Crunchie, creamy, cookie, candy, cupcake!"}
+
+    request.session.set_test_cookie()
+
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visitor_cookie_handler(request)
+
+    context_dict['visits'] = request.session['visits']
+
+    print(request.session['visits'])
+
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    return response
+
+
+def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    # To complete the exercise in chapter 4, we need to remove the following line
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
+
+    # and replace it with a pointer to ther about.html template using the render method
+    return render(request, 'rango/about.html',{})
 
 def show_category(request, category_name_slug):
-# Create a context dictionary which we can pass # to the template rendering engine. context_dict = {}
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
+    context_dict = {}
+
     try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
+        # Retrieve all of the associated pages.
+        # Note that filter() returns a list of page objects or an empty list
         pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
+        # We also add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
+
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.
     except Category.DoesNotExist:
-# We get here if we didn't find the specified category. # Don't do anything -
- # the template will display the "no category" message for us.
         context_dict['category'] = None
         context_dict['pages'] = None
+
+
+    # create a default query based on the category name
+    # to be shown in the search box
+    context_dict['query'] = category.name
+
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            # Run our Webhose function to get the results list!
+            result_list = run_query(query)
+            context_dict['query'] = query
+    context_dict['result_list'] = result_list
+
+
     # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
 
-def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-# Return a rendered response to send to the client.
-# We make use of the shortcut function to make our lives easier.
-# Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            category = form.save(commit=True)
+            print(category, category.slug)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But instead since the most recent catergory added is on the index page
+            # Then we can direct the user back to the index page.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print(form.errors)
+    # Will handle the bad form (or form details), new form or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -21,8 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-        views.show_category, name='show_category'),
-]
+url(r'^$', views.index, name='index'),
+url(r'about/$', views.about, name='about'),
+url(r'^rango/', include('rango.urls')),
+# above maps any URLs starting
+# with rango/ to be handled by
+# the rango application
+url(r'^admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
