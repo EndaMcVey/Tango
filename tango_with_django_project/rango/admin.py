36c2b191644e9589b3b815455518a98121765fd8
Enda McVey <endamcvey@Endas-MacBook-2.local>
2019-01-31 19:39:44 +0000

    Completed Chapter 8

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
 # Register your models here.
 from rango.models import Category, Page
 class CategoryAdmin(admin.ModelAdmin):
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -52,13 +52,11 @@ def index(request):
 
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
-    # To complete the exercise in chapter 4, we need to remove the following line
-    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
-    # and replace it with a pointer to ther about.html template using the render method
-    return render(request, 'rango/about.html',{})
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser` print(request.user)
+    return render(request, 'rango/about.html', {})
+
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -66,28 +64,13 @@ def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # Note that filter() returns a list of page objects or an empty list
         pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
-
-        # We get here if we didn't find the specified category.
-        # Don't do anything -
-        # the template will display the "no category" message for us.
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-    # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context_dict)
 
 
